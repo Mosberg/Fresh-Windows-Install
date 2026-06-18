e7dc2aae80626b68259acb3e72d663041e0dfc5e
Mosberg <RasmusTMosberg@gmail.com>
2026-06-18 18:09:30 +0200

    Create js/config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const SOFTWARE_SOURCES = [
+  "data/software-core.json",
+  "data/software-dev.json",
+  "data/software-gaming.json",
+  "data/software-creative.json",
+  "data/software-security.json",
+  "data/software-open-source.json"
+];
