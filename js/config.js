33620c19bab202c0399eced3793e986c345e55c7
Mosberg <RasmusTMosberg@gmail.com>
2026-06-19 18:15:56 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 const APP_VERSION = "2.0.0";
 
 const SOFTWARE_SOURCES = [
-  "https://mosberg.github.io/Fresh-Windows-Install/data/software-core.json",
-  "https://mosberg.github.io/Fresh-Windows-Install/data/software-dev.json",
-  "https://mosberg.github.io/Fresh-Windows-Install/data/software-gaming.json",
-  "https://mosberg.github.io/Fresh-Windows-Install/data/software-creative.json",
-  "https://mosberg.github.io/Fresh-Windows-Install/data/software-security.json",
-  "https://mosberg.github.io/Fresh-Windows-Install/data/software-open-source.json",
-  "https://mosberg.github.io/Fresh-Windows-Install/data/software-network.json"
+  "https://raw.githubusercontent.com/Mosberg/Fresh-Windows-Install/main/data/software-core.json",
+  "https://raw.githubusercontent.com/Mosberg/Fresh-Windows-Install/main/data/software-dev.json",
+  "https://raw.githubusercontent.com/Mosberg/Fresh-Windows-Install/main/data/software-gaming.json",
+  "https://raw.githubusercontent.com/Mosberg/Fresh-Windows-Install/main/data/software-creative.json",
+  "https://raw.githubusercontent.com/Mosberg/Fresh-Windows-Install/main/data/software-security.json",
+  "https://raw.githubusercontent.com/Mosberg/Fresh-Windows-Install/main/data/software-open-source.json",
+  "https://raw.githubusercontent.com/Mosberg/Fresh-Windows-Install/main/data/software-network.json"
 ];
