9bb20197a851c86714ffe006828539140fa32c2e
Mosberg <RasmusTMosberg@gmail.com>
2026-06-19 17:53:22 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,5 +5,5 @@ const SOFTWARE_SOURCES = [
   "https://mosberg.github.io/Fresh-Windows-Install/data/software-creative.json",
   "https://mosberg.github.io/Fresh-Windows-Install/data/software-security.json",
   "https://mosberg.github.io/Fresh-Windows-Install/data/software-open-source.json",
-  "https://mosberg.github.io/Fresh-Windows-Install/data/software-network.json
+  "https://mosberg.github.io/Fresh-Windows-Install/data/software-network.json"
 ];
